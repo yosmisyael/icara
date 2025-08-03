ff0212f9eb27e5382a26b174408253dca1775c87
yosmisyael <yosmisyael7308@gmail.com>
2025-08-03 13:44:05 +0700

    build: add mp and gson lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.icara"
-        minSdk = 22
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -41,8 +41,8 @@ android {
 }
 
 dependencies {
-    val camerax_version = "1.4.2"
 
+    // Kotlin lang
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,16 +50,32 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
+    // Material theme library
     implementation(libs.androidx.material3)
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    // Permission library
     implementation("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
+
+    // View model library
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+
+    // Camera library
+    val camerax_version = "1.4.2"
     implementation("androidx.camera:camera-core:$camerax_version")
     implementation("androidx.camera:camera-camera2:$camerax_version")
     implementation("androidx.camera:camera-lifecycle:$camerax_version")
     implementation("androidx.camera:camera-view:$camerax_version")
+
+    // Navigation library
     implementation("androidx.navigation:navigation-compose:2.9.3")
+
+    // JSON handling library
     implementation("com.google.code.gson:gson:2.10.1")
+
+    // MP library
+    implementation("com.google.mediapipe:tasks-vision:0.10.26")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
