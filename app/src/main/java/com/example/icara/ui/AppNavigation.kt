045838adc1492827b0ae9688a068a925a5e9dea2
yosmisyael <yosmisyael7308@gmail.com>
2025-08-08 11:21:02 +0700

    feat: implement onboarding and splash screen

--- app/src/main/java/com/example/icara/ui/AppNavigation.kt
+++ app/src/main/java/com/example/icara/ui/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.icara.ui
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
@@ -7,38 +8,72 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.icara.data.model.DictionaryEntry
 import com.example.icara.ui.screens.dictionary.DictionaryDetailScreen
 import com.example.icara.ui.screens.dictionary.DictionaryScreen
 import com.example.icara.ui.screens.dictionary.SharedDictionaryViewModel
 import com.example.icara.ui.screens.home.HomeScreen
+import com.example.icara.ui.screens.onboarding.WelcomeScreen
+import com.example.icara.ui.screens.splash.SplashScreen
 import com.example.icara.ui.screens.talk.TalkScreen
-import com.google.gson.Gson
+import com.example.icara.utils.PreferencesManager
 
 @Composable
-fun MyAppNavHost() {
+fun MyAppNavHost(preferencesManager: PreferencesManager) {
     val navController = rememberNavController()
     val sharedViewModel: SharedDictionaryViewModel = viewModel()
 
-    NavHost(navController = navController, startDestination = "home") {
-        // Home navigation
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") {
+            Log.d("NavHost", "Navigating to splash screen")
+            SplashScreen(
+                onNavigateNext = {
+                    val isFirstLaunch = preferencesManager.isFirstLaunch()
+
+                    val destination = if (isFirstLaunch) {
+                        "onboarding"
+                    } else {
+                        "home"
+                    }
+                    navController.navigate(destination) {
+                        // remove the splash screen from the back stack
+                        popUpTo("splash") { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        // Navigation to welcome screen
+        composable("onboarding") {
+            Log.d("NavHost", "Navigating to onboarding screen")
+            WelcomeScreen(
+                onComplete = {
+                    Log.d("NavHost", "WelcomeScreen onComplete called")
+                    preferencesManager.markOnboardingComplete()
+                    navController.navigate("home") {
+                        popUpTo("onboarding") { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        // Navigation to home screen
         composable("home") {
+            Log.d("NavHost", "Navigating to home screen")
             HomeScreen(navController = navController)
         }
 
-        // Navigation for talk screen
+        // Navigation to talk screen
         composable(
             route = "talk/{lang}",
             arguments = listOf(navArgument("lang") { type = NavType.StringType })
         ) { backStackEntry ->
             val lang = backStackEntry.arguments?.getString("lang") ?: ""
             TalkScreen(
-                lang = lang,
                 onNavigateBack = { navController.popBackStack() }
             )
         }
 
-        // Navigation for dictionary screen
+        // Navigation to dictionary screen
         composable("dictionary") {
             DictionaryScreen(
                 onNavigateBack = { navController.popBackStack() },
@@ -47,7 +82,7 @@ fun MyAppNavHost() {
             )
         }
 
-        // Navigation for dictionary entry detail
+        // Navigation to dictionary entry detail
         composable(
             route = "dictionary_detail",
         ) { backStackEntry ->
